bc55ff4a5cc7856c19237aac467841a4eabb3ebd
imac <727271755@qq.com>
2016-05-28 09:18:43 +0800

    Initial Commit

---
+++ s1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  s1
+//
+//  Created by run on 16/5/28.
+//  Copyright (c) 2016年 run. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
